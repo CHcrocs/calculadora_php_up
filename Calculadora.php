05397d1189aa543f0d6718efaed6444de4938d3a
Crocs <138597970+CHcrocs@users.noreply.github.com>
2024-05-05 17:32:29 -0300

    me ajuda pelo amor de deus

--- Calculadora.php
+++ Calculadora.php
@@ -14,7 +14,7 @@
     <label for="num2">Numero 2: </label>
     <input type="number" id="num2" name="num2">
 
-    <input type="submit" value="Calcular">
+    <input type="submit" value="Calcular" name="calcular">
 
     <input type="submit" name="salvar_memoria" value="Salvar na Memória">
     <input type="submit" name="recuperar_memoria" value="Recuperar Memória">
@@ -34,12 +34,10 @@ if (isset($_POST['apagar_historico'])) {
 
 echo "<br>";
 
-
 if (isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operacao'])) {
     $num1 = doubleval($_POST['num1']);
     $num2 = doubleval($_POST['num2']);
     $operacao = $_POST['operacao'];
-
 
     switch ($operacao) {
         case '+':
@@ -57,10 +55,8 @@ if (isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operacao']))
         case '/':
             if ($num2 != 0) {
                 $resultado = $num1 / $num2;
-
             } else {
                 $resultado = "Divisão por zero não é permitida";
-
             }
 
             break;
@@ -70,7 +66,6 @@ if (isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operacao']))
             break;
         case '!':
             $resultado = 1;
-
             while ($num1 > 1) {
                 $resultado *= $num1;
                 $num1--;
@@ -78,44 +73,54 @@ if (isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operacao']))
 
             break;
     }
-    
-    
+
+
+    if (isset($_POST['calcular'])) {
+        echo "<br> $num1 $operacao $num2 = $resultado";
+
+    }
+
     if (isset($_POST['salvar_memoria'])) {
-        // Salvar os valores na memória
-        $_SESSION['memoria'] = array($num1, $operacao, $num2);
+
+        
+        $_SESSION['memoria'] = array($num1, $operacao, $num2, $resultado);
         echo "<br>Valores salvos na memória.";
     }
-    
-    echo "<br> $num1 $operacao $num2 = $resultado";
-    
+
     if (isset($_POST['recuperar_memoria'])) {
         if (isset($_SESSION['memoria'])) {
-            list($num1, $operacao, $num2) = $_SESSION['memoria'];
-            echo "<br> $num1 $operacao $num2 = $resultado";
-            unset($_SESSION['memoria']);
 
+            list($num1, $operacao, $num2, $resultado) = $_SESSION['memoria'];
+            echo "<br> $num1 $operacao $num2 = $resultado";
+
+            unset($_SESSION['memoria']);
         } else {
+
             echo "<br>Nenhum valor na memória.";
         }
     }
 
     $historico = "$num1 $operacao $num2 = $resultado";
+
     $_SESSION['historico'][] = $historico;
 
     if (isset($_SESSION['historico'])) {
+
         echo "<h1>Histórico</h1>";
-        foreach ($_SESSION['historico'] as $op) {
-            echo $op . "<br>";
+
+        foreach (array_reverse($_SESSION['historico']) as $op) {
+            echo "<li>". $op . "</li>";
         }
+
     } else {
+
         echo "Nenhuma operação no histórico.";
         echo "<br>";
+
     }
+
 } else {
     echo "Insira valores";
 }
-
-
-echo "<br>";
 
 ?>

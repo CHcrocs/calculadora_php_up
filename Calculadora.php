9884089b15896d0822ae3c605633c5d31a3e1a5b
Crocs <138597970+CHcrocs@users.noreply.github.com>
2024-05-05 16:36:19 -0300

    histórico pronto

--- Calculadora.php
+++ Calculadora.php
@@ -15,40 +15,55 @@
     <input type="number" id="num2" name="num2">
 
     <input type="submit" value="Calcular">
+    <button type="submit" name="salvar">Salvar </button>
+    <button type="submit" name="pegar">Pegar </button>
+</form>
 
-    <input type="submit" value="Salvar" name="salvar">
-    <input type="submit" value="Pegar" name="pegar">
+<form action="" method="post">
+    <input type="hidden" name="apagar_historico" value="1">
+    <input type="submit" value="Limpar historico">
 
 </form>
 
 <?php
 session_start();
+
+if (isset($_POST['apagar_historico'])) {
+    unset($_SESSION['historico']);
+}
+
+echo "<br>";
+
+
 if (isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operacao'])) {
-    $num1 = $_POST['num1'] ?? 0;
-    $num2 = $_POST['num2'] ?? 0;
+    $num1 = doubleval($_POST['num1']);
+    $num2 = doubleval($_POST['num2']);
     $operacao = $_POST['operacao'];
+
 
     switch ($operacao) {
         case '+':
             $resultado = $num1 + $num2;
-            echo $num1 . $operacao . $num2 . " = " . $resultado;
+
             break;
         case '-':
             $resultado = $num1 - $num2;
-            echo $num1 . $operacao . $num2 . " = " . $resultado;
+
             break;
         case '*':
             $resultado = $num1 * $num2;
-            echo $num1 . $operacao . $num2 . " = " . $resultado;
-            break;
-        case '/':
-            $resultado = $num1 / $num2;
-            echo $num1 . $operacao . $num2 . " = " . $resultado;
 
             break;
+        case '/':
+            if ($num2 != 0) {
+                $resultado = $num1 / $num2;
+            } else {
+                $resultado = "Divisão por zero não é permitida";
+            }
         case '^':
             $resultado = pow($num1, $num2);
             echo $num1 . $operacao . $num2 . " = " . $resultado;
+
             break;
         case '!':
             $resultado = 1;
@@ -56,12 +71,24 @@ if (isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operacao']))
                 $resultado *= $num1;
                 $num1--;
             }
-            echo $num1 . $operacao . " = " . $resultado;
-
             break;
     }
+
+    $historico = "$num1 $operacao $num2 = $resultado";
+    $_SESSION['historico'][] = $historico;
+
+    if (isset($_SESSION['historico'])) {
+        foreach ($_SESSION['historico'] as $op) {
+            echo $op . "<br>";
+        }
+    } else {
+        echo "Nenhuma operação no histórico.";
+        echo "<br>";
+    }
 } else {
-    echo "Insira valores validos";
+    echo "Insira valores";
 }
+
+echo "<br>";
 
 ?>
